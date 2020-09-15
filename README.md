f63660ae6cebf42a6d9f27df4e7f3c2b89240700
Leo Zhang <leo@leozhang.me>
2020-09-14 23:28:05 -0700

    feat: Add grouping by tag

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # wpcat
 
 `wpcat` lists posts of Wordpress blogs.
+
+I wrote this because I wanted to make sure I didn't miss a post series from https://acoup.blog/.
+
+```
+cabal run --verbose=0 wpcat
+```
--- cmd/wpcat/Main.hs
+++ cmd/wpcat/Main.hs
@@ -1,17 +1,24 @@
 module Main (main) where
 
+import Control.Monad.Extra (sequenceUntil)
+import Data.Map (assocs, unionsWith)
+import Data.Text (dropAround)
+import Data.Time (UTCTime, zonedTimeToUTC)
+import Data.Time.Format.ISO8601 (iso8601ParseM)
 import Relude
 import Relude.Unsafe (fromJust)
-import Text.HTML.Scalpel (attr, texts, (//), text, hasClass, (@:), chroots, Scraper, scrapeURL)
-import Control.Monad.Extra (sequenceUntil)
-import Data.Time (zonedTimeToUTC, UTCTime)
-import Data.Time.Format.ISO8601 (iso8601ParseM)
-import Data.Text (dropAround)
+import Text.HTML.Scalpel (Scraper, attr, chroots, hasClass, scrapeURL, text, texts, (//), (@:))
 
 main :: IO ()
 main = do
-  pages <- concat <$> sequenceUntil null (map scrapePage [1 .. ])
-  forM_ pages print
+  posts <- concat <$> sequenceUntil null (map scrapePage [1 ..])
+  let postsByTag = unionsWith (<>) $ concatMap (\p@Post {tags} -> map (\t -> one (t, [p])) tags) posts
+
+  forM_ (assocs postsByTag) $ \(tag, postsWithTag) -> do
+    putStrLn $ "# " <> toString tag
+    forM_ (sortOn published postsWithTag) $ \Post{title} -> do
+      putStrLn $ toString title
+    putStrLn ""
   where
     scrapePage :: Int -> IO [Post]
     scrapePage n = fromJust <$> scrapeURL ("https://acoup.blog/page/" <> show n <> "/") postScraper
@@ -30,4 +37,4 @@ postScraper =
     tags <- texts $ "span" @: [hasClass "cat-links"] // "a"
     timestamp <- attr "datetime" "time"
     published <- zonedTimeToUTC <$> iso8601ParseM (toString $ dropAround (== '"') timestamp)
-    return Post { title, tags, published }
+    return Post {title, tags, published}
